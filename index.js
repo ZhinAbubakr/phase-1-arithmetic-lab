440f219a07b41fc0e8e00f6f74fca0d2af8fd0da
zhin abubakr <zheen99abubakr@gmail.com>
2024-05-04 23:05:05 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+
+// Define num1 and num2 for multiply test
+var num1 = 31; // Adjusted to ensure 20 is the highest number in the set
+var num2 = 2;
+
+// Define multiply
+var multiply = num1 * num2;
+
+// Define random for random test
+var random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 for mod test
+var num3 = 24; // Adjusted to ensure remainder is 4 when divided by num4
+var num4 = 5;
+
+// Define mod
+var mod = num3 % num4;
+
+
+var num5 = 20;
+
+var max = Math.max(12, 1, 7, 19, 20);
+
